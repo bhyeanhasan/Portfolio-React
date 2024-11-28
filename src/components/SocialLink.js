dbeef46dffa96381ad3177e93d4f21050425bafe
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2024-11-28 11:50:00 +0600

    link

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,70 +1,80 @@
 import React, {Component} from 'react';
 import './sociallinks.css';
 
+const socialLinks = [
+    {
+        name: 'GitHub',
+        url: 'https://github.com/bhyeanhasan',
+        iconClass: 'fa fa-github-square',
+        color: '#e45447',
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/bhyeanhasan/',
+        iconClass: 'fa fa-linkedin-square',
+        color: '#e45447',
+    },
+    {
+        name: 'YouTube',
+        url: 'https://www.youtube.com/@bhyeanhasan',
+        iconClass: 'fa fa-youtube-play',
+        color: '#e45447',
+    },
+    {
+        name: 'WhatsApp',
+        url: 'https://wa.me/8801920599266',
+        iconClass: 'fa fa-whatsapp',
+        color: '#e45447',
+    },
+    {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/bhyeanhasan/',
+        iconClass: 'fa fa-facebook-square',
+        color: '#e45447',
+    },
+    {
+        name: 'X',
+        url: 'https://twitter.com/bhyeanhasan',
+        iconClass: 'fa fa-twitter-square',
+        color: '#e45447',
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/bhyeanhasan/',
+        iconClass: 'fa fa-instagram',
+        color: '#e45447',
+    },
+        {
+        name: 'Blog',
+        url: 'https://bhyeanhasan.blogspot.com/',
+        iconClass: 'fa fa-globe',
+        color: '#e45447',
+    },
+];
+
 class SocialLink extends Component {
     render() {
         return (
             <section className="brands-area mb-5">
                 <div className="container mt-5">
                     <div className="brand-wrap">
-                        <div className="row align-items-center active-brand-carusel justify-content-start no-gutters">
-                            <div className="col single-brand">
-                                <h4 className="social">
-                                    <a
-                                        style={{color: '#e45447'}}
-                                        target="_blank"
-                                        href="https://github.com/bhyeanhasan"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <i className="fa fa-github-square" aria-hidden="true"></i><br/>
-                                        GitHub
-                                    </a>
-                                </h4>
-                            </div>
-
-                            <div className="col single-brand">
-                                <h4 className="social">
-                                    <a
-                                        style={{color: '#e45447'}}
-                                        target="_blank"
-                                        href="https://www.linkedin.com/in/bhyeanhasan/"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <i className="fa fa-linkedin-square" aria-hidden="true"></i><br/>
-                                        LinkedIn
-                                    </a>
-                                </h4>
-                            </div>
-
-                            <div className="col single-brand">
-                                <h4 className="social">
-                                    <a
-                                        style={{color: '#e45447'}}
-                                        target="_blank"
-                                        href="https://www.facebook.com/bhyeanhasan/"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <i className="fa fa-facebook-square" aria-hidden="true"></i><br/>
-                                        Facebook
-                                    </a>
-                                </h4>
-                            </div>
-
-                            <div className="col single-brand">
-                                <h4 className="social">
-                                    <a
-                                        style={{color: '#e45447'}}
-                                        target="_blank"
-                                        href="https://twitter.com/bhyeanhasan"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <i className="fa fa-twitter-square" aria-hidden="true"></i><br/>
-                                        Twitter
-                                    </a>
-                                </h4>
-                            </div>
-
-
+                        <div className="row align-items-center  ">
+                            {socialLinks.map((link, index) => (
+                                <div key={index} className="col single-brand my-1">
+                                    <h4 className="social">
+                                        <a
+                                            style={{color: link.color}}
+                                            target="_blank"
+                                            href={link.url}
+                                            rel="noopener noreferrer"
+                                        >
+                                            <i className={link.iconClass} aria-hidden="true"></i>
+                                            <br/>
+                                            {link.name}
+                                        </a>
+                                    </h4>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
